dc5936d501edbb3e64c67818d5be610642413308
jenkins <jenkins@kuali.co>
2017-12-07 22:48:06 -0700

    Updated license headers [ci skip]

--- rice-middleware/kew/api/src/main/java/org/kuali/rice/kew/api/util/XMLChar.java
+++ rice-middleware/kew/api/src/main/java/org/kuali/rice/kew/api/util/XMLChar.java
@@ -1,13 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the  "License");
+/**
+ * Copyright 2005-2017 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.opensource.org/licenses/ecl2.php
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
